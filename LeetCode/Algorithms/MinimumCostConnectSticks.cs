193be5952ba5f87fadc49c6a4a4ebab5f670ed99
bchariot <brucechariot@hotmail.com>
2020-08-31 17:37:30 -0400

    Updated

---
+++ LeetCode/Algorithms/MinimumCostConnectSticks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Utils;
+
+namespace LeetCode.Algorithms
+{
+    public class MinimumCostConnectSticks
+    {
+        // LeetCode #1167. Minimum Cost to Connect Sticks
+        public static void RunCode()
+        {
+            int[] sticks = new int[] { 2, 4, 3 };
+            Console.WriteLine($"    MinimumCostConnectSticks [2,4,3]: {GetMinimum(sticks)}");
+            sticks = new int[] { 1, 8, 3, 5 };
+            Console.WriteLine($"    MinimumCostConnectSticks [1,8,3,5]: {GetMinimum(sticks)}");
+        }
+
+        static int GetMinimum(int[] sticks)
+        {
+            PriorityQueue<int> queue = new PriorityQueue<int>();
+            foreach (int stick in sticks)
+            {
+                queue.Add(stick);
+            }
+
+            int cost = 0;
+            while (queue.Size() > 1)
+            {
+                int stickA = queue.Remove();
+                int stickB = queue.Remove();
+                cost += stickA + stickB;
+                queue.Add(stickA + stickB);
+            }
+
+            return cost;
+        }
+    }
+}
