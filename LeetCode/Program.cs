8e4f2238246f738a160da25635444b0e05815ef6
bchariot <brucechariot@hotmail.com>
2020-08-31 17:38:34 -0400

    Updated

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,7 +8,7 @@ namespace LeetCode
         static void Main(string[] args)
         {
             Console.WriteLine("Program started");
-            /*UniquePaths.RunCode();
+            UniquePaths.RunCode();
             PathSum.RunCode();
             QuickSelect.RunCode();
             ReverseLinkedList.RunCode();
@@ -40,7 +40,7 @@ namespace LeetCode
             ReverseVowels.RunCode();
             IntersectionTwoLinkedLists.RunCode();
             RangeSumOfBST.RunCode();
-            LastStoneWeight.RunCode();*/
+            LastStoneWeight.RunCode();
             MinimumCostConnectSticks.RunCode();
             Console.WriteLine("Program completed");
             Console.Read();
